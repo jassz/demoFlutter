5b53b5c2af6a606bed426fb080cea25f48e25494
najanadhirah <najanadhirah11@gmail.com>
2023-10-17 11:54:10 +0800

    Row and Column

---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return MaterialApp (
+    title: 'Welcome to my first Application',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Welcome to my first Application'),
+          centerTitle: true,
+          backgroundColor: Colors.red[600],
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text('Hello'),
+                Text('World!')
+              ],
+            ),
+            Container(
+              padding: EdgeInsets.all(20.0),
+              color: Colors.cyan,
+              child: Center(child: Text('one')),
+            ),
+            Container(
+              padding: EdgeInsets.all(20.0),
+              color: Colors.greenAccent,
+              child: Center(child: Text('two')),
+            ),
+            Container(
+              padding: EdgeInsets.all(20.0),
+              color: Colors.redAccent,
+              child: Center(child: Text('three')),
+            ),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () { 'test'; },
+          child: Text(
+              'click me!' ,
+              textAlign: TextAlign.center,
+          ),
+          backgroundColor: Colors.red[600],
+        ),
+      ),
+    );
+  }
+}
