37e020b6e7a78b02e4605b8ab7b179da443389b7
najanadhirah <najanadhirah11@gmail.com>
2023-10-20 16:55:34 +0800

    feat: containers, padding and margin

--- lib/main.dart
+++ lib/main.dart
@@ -17,33 +17,15 @@ class MyApp extends StatelessWidget {
           centerTitle: true,
           backgroundColor: Colors.red[600],
         ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text('Hello'),
-                Text('World!')
-              ],
+        body: Container(
+          child: Center(
+            child:  Container(
+              padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 40.0), //inside space,
+              margin: EdgeInsets.all(40.0), //outside space
+              color: Colors.grey[400], //background color for container
+              child: Text('hello all'),
             ),
-            Container(
-              padding: EdgeInsets.all(20.0),
-              color: Colors.cyanAccent,
-              child: Center(child: Text('one')),
-            ),
-            Container(
-              padding: EdgeInsets.all(20.0),
-              color: Colors.yellowAccent,
-              child: Center(child: Text('two')),
-            ),
-            Container(
-              padding: EdgeInsets.all(20.0),
-              color: Colors.redAccent,
-              child: Center(child: Text('three')),
-            ),
-          ],
+          ),
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () { 'test'; },
